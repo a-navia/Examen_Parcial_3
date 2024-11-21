072bac1e5e9676676453f1950e93101225405227
a-navia <anavimor@myuax.com>
2024-11-21 10:30:27 +0100

    Your message here

---
+++ Est.h
@@ -0,0 +1,31 @@
+#ifndef Est_H
+#define Est_H
+//definicion de constantes
+#define MAX_MATERIAS 10
+#define MAX_NOMBRE_MATERIA 50
+#define MAX_ASISTENCIAS 100
+//declaracion de la estructura asistencia
+struct Asistencia {
+    char fecha[11]; // formato "YYYY-MM-DD"
+    char materia[MAX_NOMBRE_MATERIA];
+    char estado[10]; // "asistió", "falta", "tardanza"
+};
+//declaracion de la estructura estudiante
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+    char materias[MAX_MATERIAS][MAX_NOMBRE_MATERIA];
+    int num_materias;
+    struct Asistencia asistencias[MAX_ASISTENCIAS];
+    int num_asistencias;
+};
+//funciones
+void mostrarEstudiante(struct Estudiante estudiante);
+void agregarMateria(struct Estudiante *estudiante, const char *materia);
+void eliminarMateria(struct Estudiante *estudiante, const char *materia);
+void mostrarMaterias(struct Estudiante estudiante);
+void registrarAsistencia(struct Estudiante *estudiante, const char *fecha, const char *materia, const char *estado);
+void mostrarAsistencias(struct Estudiante estudiante);
+
+#endif  //Est_H
--- main.c
+++ main.c
@@ -1,3 +1,31 @@
-//
-// Created by navia on 21-Nov-24.
-//
+#include <string.h>
+#include "Est.h"
+
+int main() {
+    struct Estudiante estudiante;
+//inicia las variables de la estructura
+    strncpy(estudiante.nombre, "Juan Perez", sizeof(estudiante.nombre) - 1);
+    estudiante.nombre[sizeof(estudiante.nombre) - 1] = '\0';
+    estudiante.edad = 20;
+    estudiante.promedio = 8.5;
+    estudiante.num_materias = 0;
+    estudiante.num_asistencias = 0;
+
+    // llama la funcion agregarMateria para agregar materias
+    agregarMateria(&estudiante, "Matematicas");
+    agregarMateria(&estudiante, "Fisica");
+    agregarMateria(&estudiante, "Quimica");
+
+    // llama la funcion registrarAsistencia para registrar asistencias
+    registrarAsistencia(&estudiante, "2023-10-01", "Matematicas", "asistio");
+    registrarAsistencia(&estudiante, "2023-10-02", "Fisica", "falta");
+    registrarAsistencia(&estudiante, "2023-10-03", "Quimica", "tardanza");
+
+    // llama la funcion mostrarEstudiante para mostrar la informacion del estudiante
+    mostrarEstudiante(estudiante);
+   //llama la funcion eliminarMateria para eliminar una materia
+    eliminarMateria(&estudiante, "Fisica");
+    //mostrar la informacion de materias
+    mostrarMaterias(estudiante);
+    return 0;
+}
